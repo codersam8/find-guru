941daa547bd2b49c34f9a1631fc6ca0c6b7dc84d
sampath surineni <sampath.surineni@oracle.com>
2017-11-21 09:11:28 +0530

    added guru model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GuruFormComponent } from './guru-form/guru-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuruFormComponent
   ],
   imports: [
     BrowserModule
